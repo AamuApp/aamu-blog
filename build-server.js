ae36f398b33f91825830b85923c7e5248066a0d1
Ilkka Huotari <ilkkah@gmail.com>
2024-04-04 06:45:52 +0300

    ...

--- build-server.js
+++ build-server.js
@@ -23,7 +23,7 @@ app.post('/2c285354-e7a8-45f4-a82f-96cb150ea9fc', (req, res) => {
     run('hugo');
     run('git add .');
     run('git commit -am "..."');
-    run('GIT_SSH_COMMAND="ssh -i /home/ile/.ssh/aamu.id_rsa git" push web');
+    run('GIT_SSH_COMMAND="ssh -i /home/ile/.ssh/aamu.id_rsa" git push web');
     run('git push origin');
 
     res.send('Site built.\n\n')
