4ff692b67b5c0533945105b934c8a391829680f2
Ilkka Huotari <ilkkah@gmail.com>
2024-04-04 05:49:21 +0300

    .......

--- build-server.js
+++ build-server.js
@@ -3,29 +3,25 @@ import { spawnSync } from 'child_process'
 const app = express()
 const port = 7777
 
+const run = (cmd, params) => {
+    result = spawnSync(cmd, params);
+
+    // Check if there was an error
+    if (result.error) {
+        console.error('Error:', result.error);
+        return;
+    }
+
+    // Print the output of the command
+    console.log(result.stdout.toString());
+}
+
 app.post('/2c285354-e7a8-45f4-a82f-96cb150ea9fc', (req, res) => {
     console.log('Got build request from', req.hostname, req.ip);
-    let result = spawnSync('npm', ['run', 'build']);
 
-    // Check if there was an error
-    if (result.error) {
-        console.error('Error:', result.error);
-        return;
-    }
-
-    // Print the output of the command
-    console.log(result.stdout.toString());
-
-    result = spawnSync('npm', ['run', 'publish']);
-
-    // Check if there was an error
-    if (result.error) {
-        console.error('Error:', result.error);
-        return;
-    }
-
-    // Print the output of the command
-    console.log(result.stdout.toString());
+    run('npm', ['run', 'build']);
+    run('npm', ['run', 'publish']);
+    run('git', ['push', 'web']);
 
     res.send('Site built.\n\n')
 })
