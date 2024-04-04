5ba502753520d1c002dd027f8efffb39da0bb646
Ilkka Huotari <ilkkah@gmail.com>
2024-04-04 05:44:09 +0300

    .......

--- build-server.js
+++ build-server.js
@@ -5,7 +5,18 @@ const port = 7777
 
 app.post('/2c285354-e7a8-45f4-a82f-96cb150ea9fc', (req, res) => {
     console.log('Got build request from', req.hostname, req.ip);
-    const result = spawnSync('npm', ['run', 'build-and-publish']);
+    let result = spawnSync('npm', ['run', 'build']);
+
+    // Check if there was an error
+    if (result.error) {
+        console.error('Error:', result.error);
+        return;
+    }
+
+    // Print the output of the command
+    console.log(result.stdout.toString());
+
+    result = spawnSync('npm', ['run', 'publish']);
 
     // Check if there was an error
     if (result.error) {
